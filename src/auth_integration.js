f855628168c4e1e925034b4df5314f97d1b83eed
suborna33 <152749142+suborna33@users.noreply.github.com>
2026-01-30 20:08:17 +0600

    polishing

---
+++ src/auth_integration.js
@@ -0,0 +1,6 @@
+/*
+1. Create a context. so that we can share the state of User across the application. 
+
+
+
+*/ 
--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,41 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createContext, StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
-import Root from './components/layouts/Root.jsx';
-import Home from './components/Home/Home.jsx';
-import Login from './components/Login/Login.jsx';
-import Register from './components/Register/Register.jsx';
+import Root from "./components/layouts/Root.jsx";
+import Home from "./components/Home/Home.jsx";
+import Login from "./components/Login/Login.jsx";
+import Register from "./components/Register/Register.jsx";
+import AuthProvider from "./contexts/AuthProvider.jsx";
 
+// export const AuthContext = createContext(null);
+
+// const userInfo = {
+//   email: 'pot@alu.com'
+// }
 
 const router = createBrowserRouter([
   {
     path: "/",
     Component: Root,
     children: [
-      {index: true, Component: Home},
-      {path: "login", Component: Login},
-      {path: "register", Component: Register}
-      
-    ]
+      { index: true, Component: Home },
+      { path: "login", Component: Login },
+      { path: "register", Component: Register },
+    ],
   },
 ]);
 
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
+    {/* <AuthContext value={userInfo}>
     <RouterProvider router={router}/>
+    </AuthContext> */}
+
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>,
-)
+);
